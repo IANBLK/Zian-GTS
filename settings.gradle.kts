db1e8e5ef12a780a888eee4e6e673405c5af8cc0
IANBLK <161622134+IANBLK@users.noreply.github.com>
2026-09-19 18:07:16 -0500

    build: add Modrinth Maven repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,14 +5,22 @@ pluginManagement {
         maven("https://maven.minecraftforge.net")
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         mavenCentral()
         maven("https://maven.neoforged.net/releases")
-        maven("https://maven.impactdev.net/repository/development/")
-        maven("https://maven.impactdev.net/repository/releases/")
         maven("https://thedarkcolour.github.io/KotlinForForge/")
+        exclusiveContent {
+            forRepository {
+                maven("https://api.modrinth.com/maven") {
+                    name = "Modrinth"
+                }
+            }
+            filter { includeGroup("maven.modrinth") }
+        }
     }
 }
+
 rootProject.name = "Zian-GTS"
