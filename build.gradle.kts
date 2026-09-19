4a86fdea62b124864bb8b67854986c611f198147
IANBLK <161622134+IANBLK@users.noreply.github.com>
2026-09-19 18:10:13 -0500

    build: switch to official Cobblemon NeoForge artifact

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     java
     id("net.neoforged.moddev") version "2.0.107"
@@ -7,8 +10,13 @@ plugins {
 group = property("mod_group_id") as String
 version = property("mod_version") as String
 
+base {
+    archivesName.set("zian-gts")
+}
+
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    withSourcesJar()
 }
 
 neoForge {
@@ -33,19 +41,30 @@ repositories {
     mavenCentral()
     maven("https://maven.neoforged.net/releases")
     maven("https://thedarkcolour.github.io/KotlinForForge/")
+    maven("https://maven.impactdev.net/repository/development/")
 }
 
 dependencies {
     implementation("thedarkcolour:kotlinforforge-neoforge:${property("kotlin_for_forge_version")}")
-    implementation("maven.modrinth:cobblemon:${property("cobblemon_version")}")
+    implementation("com.cobblemon:neoforge:${property("cobblemon_version")}") {
+        isTransitive = false
+    }
+}
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    filesMatching("META-INF/neoforge.mods.toml") {
+        expand("version" to project.version)
+    }
 }
 
 tasks.withType<JavaCompile>().configureEach {
     options.encoding = "UTF-8"
+    options.release.set(21)
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
 }
 
 tasks.jar {
