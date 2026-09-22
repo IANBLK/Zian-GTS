27ca922790b7d45aa3dfa8d4d066ba0fc65a600c
IANBLK <161622134+IANBLK@users.noreply.github.com>
2026-09-22 12:23:00 -0500

    fix(v2): repair NeoForge lifecycle wiring syntax

--- src/main/kotlin/com/zianblk/ziangts/ZianGts.kt
+++ src/main/kotlin/com/zianblk/ziangts/ZianGts.kt
@@ -1,13 +1,16 @@
 package com.zianblk.ziangts
 
+import com.zianblk.ziangts.config.GtsSettings
+import com.zianblk.ziangts.server.GtsCommands
+import com.zianblk.ziangts.v2.runtime.ZianGtsV2Runtime
+import net.neoforged.fml.ModContainer
 import net.neoforged.fml.common.Mod
+import net.neoforged.fml.config.ModConfig
 import net.neoforged.neoforge.common.NeoForge
 import net.neoforged.neoforge.event.RegisterCommandsEvent
-import com.zianblk.ziangts.server.GtsCommands
+import net.neoforged.neoforge.event.server.ServerStartedEvent
+import net.neoforged.neoforge.event.server.ServerStoppingEvent
 import org.slf4j.LoggerFactory
-import net.neoforged.fml.ModContainer
-import net.neoforged.fml.config.ModConfig
-import com.zianblk.ziangts.config.GtsSettings\nimport com.zianblk.ziangts.v2.runtime.ZianGtsV2Runtime\nimport net.neoforged.neoforge.event.server.ServerStartedEvent\nimport net.neoforged.neoforge.event.server.ServerStoppingEvent
 
 @Mod(ZianGts.MOD_ID)
 class ZianGts(container: ModContainer, bus: net.neoforged.bus.api.IEventBus) {
@@ -17,7 +20,11 @@ class ZianGts(container: ModContainer, bus: net.neoforged.bus.api.IEventBus) {
         NeoForge.EVENT_BUS.addListener { event: RegisterCommandsEvent -> GtsCommands.register(event.dispatcher) }
         NeoForge.EVENT_BUS.addListener(com.zianblk.ziangts.server.GtsJournal::started)
         NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST, com.zianblk.ziangts.server.GtsJournal::stopping)
-        NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST, com.zianblk.ziangts.server.GtsJournal::stopped)\n        NeoForge.EVENT_BUS.addListener { event: ServerStartedEvent -> ZianGtsV2Runtime.start(event.server) }\n        NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST) { event: ServerStoppingEvent -> ZianGtsV2Runtime.stop(event.server) }
+        NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST, com.zianblk.ziangts.server.GtsJournal::stopped)
+        NeoForge.EVENT_BUS.addListener { event: ServerStartedEvent -> ZianGtsV2Runtime.start(event.server) }
+        NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST) { event: ServerStoppingEvent ->
+            ZianGtsV2Runtime.stop(event.server)
+        }
         LOGGER.info("Inicializando Zian GTS")
     }
 
