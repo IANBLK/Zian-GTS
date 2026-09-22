5fda49cd0c8a581a574da019acac7ae93a77e300
IANBLK <161622134+IANBLK@users.noreply.github.com>
2026-09-22 12:25:38 -0500

    feat(v2): register runtime validation commands

--- src/main/kotlin/com/zianblk/ziangts/ZianGts.kt
+++ src/main/kotlin/com/zianblk/ziangts/ZianGts.kt
@@ -3,6 +3,7 @@ package com.zianblk.ziangts
 import com.zianblk.ziangts.config.GtsSettings
 import com.zianblk.ziangts.server.GtsCommands
 import com.zianblk.ziangts.v2.runtime.ZianGtsV2Runtime
+import com.zianblk.ziangts.v2.runtime.V2TestCommands
 import net.neoforged.fml.ModContainer
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.config.ModConfig
@@ -17,7 +18,10 @@ class ZianGts(container: ModContainer, bus: net.neoforged.bus.api.IEventBus) {
     init {
         bus.addListener(com.zianblk.ziangts.network.GtsNetwork::register)
         container.registerConfig(ModConfig.Type.SERVER, GtsSettings.spec)
-        NeoForge.EVENT_BUS.addListener { event: RegisterCommandsEvent -> GtsCommands.register(event.dispatcher) }
+        NeoForge.EVENT_BUS.addListener { event: RegisterCommandsEvent ->
+            GtsCommands.register(event.dispatcher)
+            V2TestCommands.register(event.dispatcher)
+        }
         NeoForge.EVENT_BUS.addListener(com.zianblk.ziangts.server.GtsJournal::started)
         NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST, com.zianblk.ziangts.server.GtsJournal::stopping)
         NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST, com.zianblk.ziangts.server.GtsJournal::stopped)
