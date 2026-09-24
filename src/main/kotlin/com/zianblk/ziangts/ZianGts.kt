d06b45979fb1b138f0ba5b09409c4226340f856f
IANBLK <161622134+IANBLK@users.noreply.github.com>
2026-09-24 03:13:09 -0500

    refactor(v2): register production command surface

--- src/main/kotlin/com/zianblk/ziangts/ZianGts.kt
+++ src/main/kotlin/com/zianblk/ziangts/ZianGts.kt
@@ -3,7 +3,7 @@ package com.zianblk.ziangts
 import com.zianblk.ziangts.config.GtsSettings
 import com.zianblk.ziangts.server.GtsCommands
 import com.zianblk.ziangts.v2.runtime.ZianGtsV2Runtime
-import com.zianblk.ziangts.v2.runtime.V2TestCommands
+import com.zianblk.ziangts.v2.runtime.V2Commands
 import net.neoforged.fml.ModContainer
 import net.neoforged.fml.common.Mod
 import net.neoforged.fml.config.ModConfig
@@ -21,7 +21,7 @@ class ZianGts(container: ModContainer, bus: net.neoforged.bus.api.IEventBus) {
         container.registerConfig(ModConfig.Type.SERVER, GtsSettings.spec)
         NeoForge.EVENT_BUS.addListener { event: RegisterCommandsEvent ->
             GtsCommands.register(event.dispatcher)
-            V2TestCommands.register(event.dispatcher)
+            V2Commands.register(event.dispatcher)
         }
         NeoForge.EVENT_BUS.addListener(com.zianblk.ziangts.server.GtsJournal::started)
         NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST, com.zianblk.ziangts.server.GtsJournal::stopping)
