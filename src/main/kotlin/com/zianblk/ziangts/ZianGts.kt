47241f6c2e7bd25f0e212900571bd199823194d7
IANBLK <161622134+IANBLK@users.noreply.github.com>
2026-09-24 03:23:22 -0500

    cleanup(v2): remove V1 runtime registration

--- src/main/kotlin/com/zianblk/ziangts/ZianGts.kt
+++ src/main/kotlin/com/zianblk/ziangts/ZianGts.kt
@@ -1,12 +1,9 @@
 package com.zianblk.ziangts
 
-import com.zianblk.ziangts.config.GtsSettings
-import com.zianblk.ziangts.server.GtsCommands
 import com.zianblk.ziangts.v2.runtime.ZianGtsV2Runtime
 import com.zianblk.ziangts.v2.runtime.V2Commands
 import net.neoforged.fml.ModContainer
 import net.neoforged.fml.common.Mod
-import net.neoforged.fml.config.ModConfig
 import net.neoforged.neoforge.common.NeoForge
 import net.neoforged.neoforge.event.RegisterCommandsEvent
 import net.neoforged.neoforge.event.server.ServerStartedEvent
@@ -16,16 +13,10 @@ import org.slf4j.LoggerFactory
 @Mod(ZianGts.MOD_ID)
 class ZianGts(container: ModContainer, bus: net.neoforged.bus.api.IEventBus) {
     init {
-        bus.addListener(com.zianblk.ziangts.network.GtsNetwork::register)
         bus.addListener(com.zianblk.ziangts.v2.network.V2MarketNetwork::register)
-        container.registerConfig(ModConfig.Type.SERVER, GtsSettings.spec)
         NeoForge.EVENT_BUS.addListener { event: RegisterCommandsEvent ->
-            GtsCommands.register(event.dispatcher)
             V2Commands.register(event.dispatcher)
         }
-        NeoForge.EVENT_BUS.addListener(com.zianblk.ziangts.server.GtsJournal::started)
-        NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST, com.zianblk.ziangts.server.GtsJournal::stopping)
-        NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST, com.zianblk.ziangts.server.GtsJournal::stopped)
         NeoForge.EVENT_BUS.addListener { event: ServerStartedEvent -> ZianGtsV2Runtime.start(event.server) }
         NeoForge.EVENT_BUS.addListener(net.neoforged.bus.api.EventPriority.LOWEST) { event: ServerStoppingEvent ->
             ZianGtsV2Runtime.stop(event.server)
